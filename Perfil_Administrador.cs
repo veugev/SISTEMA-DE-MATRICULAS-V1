d99e94e31c86badb9485c6349af4615de5494a37
Veugev <victoria.vp.ipchile@gmail.com>
2018-11-11 18:29:47 -0300

    Actualizacion 3

---
+++ Perfil_Administrador.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SISTEMA_DE_MATRICULA_V1
+{
+    public partial class Perfil_Administrador : Form
+    {
+        public Perfil_Administrador()
+        {
+            InitializeComponent();
+            
+            button1.BackgroundImage = Properties.Resources.Btn_Celeste;
+            button2.BackgroundImage = Properties.Resources.Btn_Celeste;
+            button3.BackgroundImage = Properties.Resources.Btn_Celeste;
+            button4.BackgroundImage = Properties.Resources.Btn_Celeste;
+            button5.BackgroundImage = Properties.Resources.Btn_Celeste;
+
+            this.MaximizeBox = false;
+
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+        }
+
+        private void Perfil_Soporte_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Administradores cambiarformAd = new Administradores();
+            cambiarformAd.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            BdConexion.ObtenerConexion();
+            MessageBox.Show("conectado exitosamente");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            button1.BackgroundImage = Properties.Resources.Btn_Verde;
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.BackgroundImage = Properties.Resources.Btn_Celeste;
+        }
+
+        private void button2_MouseMove(object sender, MouseEventArgs e)
+        {
+            button2.BackgroundImage = Properties.Resources.Btn_Verde;
+        }
+
+        private void button2_MouseLeave(object sender, EventArgs e)
+        {
+            button2.BackgroundImage = Properties.Resources.Btn_Celeste;
+        }
+
+        private void button3_MouseMove(object sender, MouseEventArgs e)
+        {
+            button3.BackgroundImage = Properties.Resources.Btn_Verde;
+        }
+
+        private void button3_MouseLeave(object sender, EventArgs e)
+        {
+            button3.BackgroundImage = Properties.Resources.Btn_Celeste;
+        }
+
+        private void button4_MouseMove(object sender, MouseEventArgs e)
+        {
+            button4.BackgroundImage = Properties.Resources.Btn_Verde;
+        }
+
+        private void button4_MouseLeave(object sender, EventArgs e)
+        {
+            button4.BackgroundImage = Properties.Resources.Btn_Celeste;
+        }
+
+        private void button5_MouseMove(object sender, MouseEventArgs e)
+        {
+            button5.BackgroundImage = Properties.Resources.Btn_Verde;
+        }
+
+        private void button5_MouseLeave(object sender, EventArgs e)
+        {
+            button5.BackgroundImage = Properties.Resources.Btn_Celeste;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            Administradores entrarAgregarUs = new Administradores();
+            entrarAgregarUs.Show();
+        }
+    }
+}
